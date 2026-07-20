0ab438a2c9d82571cafa76b8cbf10251b93b6c33
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-20 23:26:22 +0800

    Create diy_x86_pang.sh

---
+++ diy_x86_pang.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
+sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
