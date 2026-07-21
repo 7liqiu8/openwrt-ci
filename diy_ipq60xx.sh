c4e6a3298e1a450cf89c08170bcbe7097044c2b6
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-21 09:15:00 +0800

    Update diy_ipq60xx.sh

--- diy_ipq60xx.sh
+++ diy_ipq60xx.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# 添加sonic_fullcone
-curl -sSL -o add_sonic_fullcone.sh \
-  https://raw.githubusercontent.com/mufeng05/openwrt-sonic-fullcone/master/add_sonic_fullcone.sh
-  
-chmod +x add_sonic_fullcone.sh
-
-./add_sonic_fullcone.sh
-
 # 修改默认 IP 和主机名
 sed -i 's/192.168.1.1/10.10.21.1/g' package/base-files/files/bin/config_generate
