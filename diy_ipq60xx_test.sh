459a0853df915962803c69435e6377cb09ac9410
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-21 17:43:59 +0800

    Update diy_ipq60xx_test.sh

--- diy_ipq60xx_test.sh
+++ diy_ipq60xx_test.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# 修改 Makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 #  Fullcone 补丁
 echo "正在应用 Fullcone NAT 补丁..."
 curl -sSL -o add_sonic_fullcone.sh \
