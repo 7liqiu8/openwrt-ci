de30f1c96aba8e7d2c2a64e5254131d58630690a
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-21 17:07:45 +0800

    Update diy_x86_pang.sh

--- diy_x86_pang.sh
+++ diy_x86_pang.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
