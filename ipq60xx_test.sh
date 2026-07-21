0f14575f561af120f00d18de4557720889138ccc
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-21 17:41:39 +0800

    Create ipq60xx_test.sh

---
+++ ipq60xx_test.sh
@@ -0,0 +1,8 @@
+#  Fullcone 补丁
+echo "正在应用 Fullcone NAT 补丁..."
+curl -sSL -o add_sonic_fullcone.sh \
+  https://raw.githubusercontent.com/mufeng05/openwrt-sonic-fullcone/master/add_sonic_fullcone.sh
+  
+chmod +x add_sonic_fullcone.sh
+
+./add_sonic_fullcone.sh
