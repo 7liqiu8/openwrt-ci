07ea714ea79ffd061d56f59338a1f68512071e5e
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-24 01:32:14 +0800

    Update mir3g_part1.sh

--- diy/mir3g_part1.sh
+++ diy/mir3g_part1.sh
@@ -8,9 +8,5 @@
 find feeds -type d -name "lucky" -exec rm -rf {} \; 2>/dev/null
 find package/feeds -type l -name "lucky" -delete 2>/dev/null
 
-# 删除 argon
-find feeds -type d \( -name "luci-theme-argon" -o -name "luci-app-argon-config" \) -exec rm -rf {} \; 2>/dev/null
-find package/feeds -type l \( -name "luci-theme-argon" -o -name "luci-app-argon-config" \) -delete 2>/dev/null
-
 ./scripts/feeds update -i
 ./scripts/feeds install -a
