e575a881ae1ff65bfb7df2472083964d23d078df
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-24 01:30:57 +0800

    Update x86_zhu_part1.sh

--- diy/x86_zhu_part1.sh
+++ diy/x86_zhu_part1.sh
@@ -17,9 +17,6 @@ find package/feeds -type l \( -name "mwan3" -o -name "luci-app-mwan3" -o -name "
 find feeds -type d -name "lucky" -exec rm -rf {} \; 2>/dev/null || true
 find package/feeds -type l -name "lucky" -delete 2>/dev/null || true
 
-# 删除 argon
-find feeds -type d \( -name "luci-theme-argon" -o -name "luci-app-argon-config" \) -exec rm -rf {} \; 2>/dev/null || true
-find package/feeds -type l \( -name "luci-theme-argon" -o -name "luci-app-argon-config" \) -delete 2>/dev/null || true
 
 echo "===== 结束 检测并删除旧或自带插件 ====="
 
