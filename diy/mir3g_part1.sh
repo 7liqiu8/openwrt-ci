ecb918cbefdff8ff6696e8c755cbc286c5200ccd
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-24 16:34:35 +0800

    Update mir3g_part1.sh

--- diy/mir3g_part1.sh
+++ diy/mir3g_part1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # set -e
 
+echo "===== 开始修改feeds.conf.default ====="
+
+# 如果 feeds.conf.default 中还没有 passwall_packages，则追加
+if ! grep -q "passwall_packages" feeds.conf.default; then
+    echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> feeds.conf.default
+fi
+
+echo "===== 结束修改feeds.conf.default ====="
+
 ./scripts/feeds update -a
 
 
