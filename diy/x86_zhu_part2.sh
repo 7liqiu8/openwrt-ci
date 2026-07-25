d8fd57fd2fa0af24925b3a784360b84882608777
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-26 02:58:53 +0800

    Update x86_zhu_part2.sh

--- diy/x86_zhu_part2.sh
+++ diy/x86_zhu_part2.sh
@@ -26,7 +26,7 @@ echo "===== 结束 拉取额外插件 ====="
 echo "===== 开始  SONiC 补丁并运行====="
 
 #  删除冲突的 firewall4 旧补丁
-rm -f package/network/config/firewall4/patches/001-firewall4-add-support-for-fullcone-nat.patch
+# rm -f package/network/config/firewall4/patches/001-firewall4-add-support-for-fullcone-nat.patch
 
 #  运行 SONiC 补丁脚本
 curl -sSL -o add_sonic_fullcone.sh \
