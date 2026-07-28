7214ba3d2f03642b92df25a8663ad9d644a993b7
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-29 01:39:13 +0800

    Create x86_fanchmwrt_part2_1.sh

---
+++ diy/diy/x86_fanchmwrt_part2_1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+echo "===== 开始 拉取额外插件 ====="
+
+# 添加 mwan3 核心包
+git clone --depth=1 https://github.com/dl12345/mwan3.git package/mwan3 || exit 1
+sed -i 's/libnetfilter_conntrack/libnetfilter-conntrack/g' package/mwan3/Makefile || exit 1
+git clone --depth=1 https://github.com/dl12345/luci-app-mwan3.git package/luci-app-mwan3 || exit 1
+
+cat >> package/luci-app-mwan3/Makefile <<'EOF'
+
+$(eval $(call BuildPackage,luci-app-mwan3))
+EOF
+
+echo "===== 结束 拉取mwan3 ====="
+
+# 添加 lucky
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky || exit 1
+
+echo "===== 结束 拉取lucky ====="
+
+echo "===== 结束 拉取额外插件 ====="
+
+echo "===== 开始  turbo acc 补丁并运行====="
+
+#  删除冲突的 firewall4 旧补丁
+# rm -f package/network/config/firewall4/patches/001-firewall4-add-support-for-fullcone-nat.patch
+# rm -rf package/network/utils/fullconenat-nft
+
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+echo "===== 结束 turbo acc 补丁 ====="
+
+
+#  修改 IP 和主机名
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+#sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
