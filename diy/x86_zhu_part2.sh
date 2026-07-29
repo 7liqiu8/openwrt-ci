f987d073638e09113074be5db6b4f7ecee0950d9
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-07-29 11:21:16 +0800

    Update x86_zhu_part2.sh

--- diy/x86_zhu_part2.sh
+++ diy/x86_zhu_part2.sh
@@ -23,6 +23,10 @@ git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky |
 
 echo "===== 结束 拉取lucky ====="
 
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+echo "===== 结束 OpenAppFilter ====="
+
 echo "===== 结束 拉取额外插件 ====="
 
 echo "===== 开始  turbo acc 补丁并运行====="
