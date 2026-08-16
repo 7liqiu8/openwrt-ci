d43de95981650e5c164409dc03d5f7e350d01f8b
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-08-16 18:19:30 +0800

    Add EasyTier package cloning to script

--- diy/mir3g_part2.sh
+++ diy/mir3g_part2.sh
@@ -9,6 +9,9 @@ git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky |
 
 echo "===== 结束 拉取lucky ====="
 
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier || exit 1
+
+echo "===== 结束 拉取easytier ====="
 
 #  修改 IP 和主机名
 sed -i 's/192.168.1.1/10.10.60.1/g' package/base-files/files/bin/config_generate
