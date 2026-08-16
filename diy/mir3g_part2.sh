4d23a307ab68f2c63bf6dbcabd636db9bccb9259
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-08-16 19:04:39 +0800

    Update mir3g_part2.sh

--- diy/mir3g_part2.sh
+++ diy/mir3g_part2.sh
@@ -13,6 +13,9 @@ git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/lu
 
 echo "===== 结束 拉取easytier ====="
 
+# 运行 turboacc 集成脚本（它会复制所有补丁，包括我们不需要的 952/953/613）
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 #  修改 IP 和主机名
 sed -i 's/192.168.1.1/10.10.60.1/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
