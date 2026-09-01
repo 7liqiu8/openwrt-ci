24bc811068311bff99c2756b0aa7a7c7ec52010f
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-09-01 21:06:26 +0800

    Update x86_pang_part1.sh

--- diy/x86_pang_part1.sh
+++ diy/x86_pang_part1.sh
@@ -9,7 +9,7 @@ echo "===== 结束 ./scripts/feeds update -a ====="
 
 echo "===== 开始 检测并删除旧或自带插件 ====="
 
-# 删除 feeds 中所有 zerotier 相关目录（本体 + Luci + 依赖）
+# 删除  zerotier 相关
 # find feeds -type d \( -name "zerotier" -o -name "luci-app-zerotier" \) -exec rm -rf {} \; 2>/dev/null || true
 # 如果之前编译过，也删除 package/feeds 里的符号链接
 # find package/feeds -type l \( -name "zerotier" -o -name "luci-app-zerotier" \) -delete 2>/dev/null || true
@@ -21,6 +21,18 @@ find package/feeds -type l \( -name "mwan3" -o -name "luci-app-mwan3" -o -name "
 # 删除 lucky
 find feeds -type d -name "lucky" -exec rm -rf {} \; 2>/dev/null || true
 find package/feeds -type l -name "lucky" -delete 2>/dev/null || true
+
+# 删除 passwall 相关
+# find feeds -type d \( -name "passwall" -o -name "luci-app-passwall" \) -exec rm -rf {} \; 2>/dev/null || true
+# find package/feeds -type l \( -name "passwall" -o -name "luci-app-passwall" \) -delete 2>/dev/null || true
+
+# 删除 passwall2 相关
+# find feeds -type d \( -name "passwall2" -o -name "luci-app-passwall2" \) -exec rm -rf {} \; 2>/dev/null || true
+# find package/feeds -type l \( -name "passwall2" -o -name "luci-app-passwall2" \) -delete 2>/dev/null || true
+
+# 删除 openclash 相关
+# find feeds -type d \( -name "openclash" -o -name "luci-app-openclash" \) -exec rm -rf {} \; 2>/dev/null || true
+# find package/feeds -type l \( -name "openclash" -o -name "luci-app-openclash" \) -delete 2>/dev/null || true
 
 # 删除 turboacc 相关包（避免与自定义 package/turboacc 冲突）
 find feeds -type d \( -name "luci-app-turboacc" -o -name "fullconenat" -o -name "fullconenat-nft" -o -name "shortcut-fe" -o -name "turboacc" \) -exec rm -rf {} \; 2>/dev/null || true
