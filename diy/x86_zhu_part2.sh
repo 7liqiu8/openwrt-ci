078ccd98d36121be4fdb4feb421b660d0c7b14bb
7liqiu8 <118182628+7liqiu8@users.noreply.github.com>
2026-09-02 12:23:25 +0800

    Update x86_zhu_part2.sh

--- diy/x86_zhu_part2.sh
+++ diy/x86_zhu_part2.sh
@@ -46,9 +46,9 @@ echo "===== 开始  turbo acc 补丁并运行 ====="
 # 同时删除 shortcut-fe 用户态软件包（避免编译时选择它）
 # rm -rf package/turboacc/shortcut-fe
 
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
-echo "===== 结束 turbo acc 补丁 ====="
+# echo "===== 结束 turbo acc 补丁 ====="
 
 
 #  修改 IP 和主机名
